396ec4ea932b981374394472387db9a1bae08d27
skywind3000@gmail.com <skywind3000@gmail.com@a3833e02-c95a-7a0c-6975-7629b7ad1a9b>
2013-04-13 13:37:38 +0000

    commit new source

--- trunk/ikcp.h
+++ trunk/ikcp.h
@@ -1,9 +1,12 @@
 //=====================================================================
 //
-// ikcp.h - fast ARQ protocol implementation
-//
-// NOTE:
-// for more information, please see the readme file
+// KCP - A Better ARQ Protocol Implementation
+// skywind3000 (at) gmail.com, 2010-2011
+//  
+// Features:
+// + Average RTT reduce 30% - 40% vs traditional ARQ like tcp.
+// + Maximum RTT reduce three times vs tcp.
+// + Lightweight, distributed as a single source file.
 //
 //=====================================================================
 #ifndef __IKCP_H__
